37501c8b15b3208afa077a4d25148cd957f846fa
imevasoldi <imevasoldi@gmail.com>
2022-12-06 14:33:25 -0600

    adding buttons

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -2,6 +2,11 @@ function Info () {
     return <div>
         <h1 className="info--title">Juan J. Mejía</h1>
         <h2 className="info--subtitle">FrontEnd Developer</h2>
+        <h3 className="website--link"> juan.mejia.website</h3>
+        <div className="buttons--">
+            <button className="button--mail">Email</button>
+            <button className="button--linkedin">LinkedIn</button>
+        </div>
         </div>
 }
 
