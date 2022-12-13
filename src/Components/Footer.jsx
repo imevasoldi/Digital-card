3da0ace4a67038c8fc2a448dce8fa173aa4aa491
imevasoldi <imevasoldi@gmail.com>
2022-12-13 17:09:15 -0600

    footer background

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,7 @@
 export const Footer = () => {
     return (
     <div>
-            {/* <h1 style={{background: "red"}}>h1</h1>
-            <h2 style={{background: "blue"}}>h2</h2>
-            <h3 style={{background: "blue"}}>y h3</h3> */}
-        <div className="footer--">
+        <div style={{borderRadius: "0px 0px 10px 10px"}} className="footer--">
             <a href="https://github.com/imevasoldi" target="_blank">
             <i style={{fontSize: "35px"}} className="github icon"/>
             </a>
