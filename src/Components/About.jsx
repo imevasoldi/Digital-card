5ccf1129ff6f80ea45e4359ed22f2e8d37fd2a98
imevasoldi <imevasoldi@gmail.com>
2023-03-10 13:30:09 -0600

    about update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,7 +1,10 @@
 export default function About(){
     return <div className="about--">
         <h2 style={{margin: "0", color: "#F5F5F5"}}>About</h2>
-        <p style={{color: "#DCDCDC"}}>I have 5 years developing web applications. I have experience working with <strong>Remix Fullstack components</strong>, code standardization and automated version control. 
+        <p style={{color: "#DCDCDC"}}>I have 5 years developing web applications. I have experience working with
+Node js and Remix Fullstack components, code standardization and
+automated version control. Additionally, I have worked with tools such as Jira,
+Clickup, Figma, Postman, git and github. 
         </p>
     </div>
 }
