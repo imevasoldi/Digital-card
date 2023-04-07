5b6ae8aaccb6d2418eaefec8ba531a938f85e69a
imevasoldi <imevasoldi@gmail.com>
2023-04-07 16:27:54 -0500

    C2 english level

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,10 +1,11 @@
 export default function About(){
     return <div className="about--">
         <h2 style={{margin: "0", color: "#F5F5F5"}}>About</h2>
-        <p style={{color: "#DCDCDC"}}>I have 5 years developing web applications. I have experience working with
-Node js and Remix Fullstack components, code standardization and
-automated version control. Additionally, I have worked with tools such as Jira,
-Clickup, Figma, Postman, git and github. 
+        <p style={{color: "#DCDCDC"}}>I’m a javascript developer with C2(advanced) english level. I have 5 years
+developing web applications. I have experience working with Node js and Remix
+Fullstack components, code standardization and automated version control.
+Additionally, I have worked with tools such as Jira, Miro, Clickup, Figma,
+Postman, git and github. 
         </p>
     </div>
 }
